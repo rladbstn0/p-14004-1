a9b746d9314f19661d33c47f90093e7554164bba
younsoo <younsoo6103@naver.com>
2025-08-20 15:16:18 +0900

    work

--- backend/src/main/kotlin/com/back/domain/post/post/repository/PostRepositroyImpl.kt
+++ backend/src/main/kotlin/com/back/domain/post/post/repository/PostRepositroyImpl.kt
@@ -1,4 +1,60 @@
 package com.back.domain.post.post.repository
 
-class PostRepositroyImpl : PostRepositoryCustom {
+import com.back.domain.post.post.entity.Post
+import com.back.domain.post.post.entity.QPost
+import com.back.standard.dto.PostSearchKeywordType1
+import com.back.standard.util.QueryDslUtil
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.support.PageableExecutionUtils
+
+class PostRepositoryImpl(
+    private val queryFactory: JPAQueryFactory
+) : PostRepositoryCustom {
+    override fun findQPagedByKw(kwType: PostSearchKeywordType1, kw: String, pageable: Pageable): Page<Post> {
+        val post = QPost.post
+
+        val builder = com.querydsl.core.BooleanBuilder()
+
+        if (kw.isNotBlank()) {
+            when (kwType) {
+                PostSearchKeywordType1.TITLE -> builder.and(post.title.containsIgnoreCase(kw))
+                PostSearchKeywordType1.CONTENT -> builder.and(post.content.containsIgnoreCase(kw))
+                PostSearchKeywordType1.ALL ->
+                    builder.and(
+                        post.title.containsIgnoreCase(kw)
+                            .or(post.content.containsIgnoreCase(kw))
+                    )
+            }
+        }
+
+        val query = queryFactory
+            .selectFrom(post)
+            .where(builder)
+
+        QueryDslUtil.applySorting(query, pageable) { property ->
+            when (property) {
+                "id" -> post.id
+                "title" -> post.title
+                "content" -> post.content
+                else -> null
+            }
+        }
+
+        val results = query
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .fetch()
+
+        val totalQuery = queryFactory
+            .select(post.count())
+            .from(post)
+            .where(builder)
+
+        return PageableExecutionUtils.getPage(results, pageable) {
+            totalQuery.fetchFirst() ?: 0L
+        }
+
+    }
 }
---
+++ backend/src/main/kotlin/com/back/standard/dto/PostSearchKeywordType1.kt
@@ -0,0 +1,7 @@
+package com.back.standard.dto
+
+enum class PostSearchKeywordType1 {
+    TITLE,
+    CONTENT,
+    ALL
+}
